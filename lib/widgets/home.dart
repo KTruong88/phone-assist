d581907d60f0d8088286db038288c700cd087770
ydocforr <crforrest@smcm.edu>
2019-01-13 10:42:05 -0500

    GUI & Arch Updates
    Changed up module creation and module manager for possible future updates. Added Left and Right Drawers for MISC Pages and Actions. Added GUI for Dark Theme, not implemented yet.

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,40 +1,37 @@
 import 'package:flutter/material.dart';
-
-import '../factories/widget_factory.dart';
 import './bottom_navbar.dart';
+import './module_manager.dart';
+import './app_settings_drawer.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _Home createState() => new _Home();
+}
+
+class _Home extends State<Home> {
+  var _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
-    WidgetFactory widgetFactory = new WidgetFactory();
-    Widget module = widgetFactory.createWidget('module', 'Module');
-    Widget marcoPolo = widgetFactory.createWidget('MarcoPolo', 'Marco Polo');
-    Widget keepOff = widgetFactory.createWidget('KeepOff', 'Keep Off');
-    Widget fireTest = widgetFactory.createWidget('FireTest', 'Fire Test');
-
     return Scaffold(
-        appBar: AppBar(
+        key: _scaffoldKey,
+        drawer: new Drawer(),
+        endDrawer: new AppSettingsDrawer(),
+        appBar: new AppBar(
+          centerTitle: true,
           title: Text('Phone Assist'),
-        ),
-        body: CustomScrollView(
-          primary: false,
-          slivers: <Widget>[
-            SliverPadding(
-              padding: const EdgeInsets.all(20.0),
-              sliver: SliverGrid.count(
-                crossAxisSpacing: 10.0,
-                mainAxisSpacing: 6.0,
-                crossAxisCount: 2,
-                children: <Widget>[
-                  module,
-                  marcoPolo,
-                  keepOff,
-                  fireTest
-                ],
+          actions: <Widget>[
+            new Container(
+              child: new IconButton(
+                icon: Icon(Icons.settings),
+                onPressed: () {
+                  _scaffoldKey.currentState.openEndDrawer(); // right side
+                },
               ),
             ),
           ],
         ),
+        body: ModuleManager(),
         bottomNavigationBar: BottomNavbar());
   }
 }
---
+++ lib/widgets/module_manager.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../modules/module.dart';
+import './modules.dart';
+import '../factories/widget_factory.dart';
+
+class ModuleManager extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ModuleManagerState();
+  }
+}
+
+class _ModuleManagerState extends State<ModuleManager> {
+  List<Module> _modules = [];
+  @override
+  void initState() {
+    super.initState();
+    WidgetFactory widgetFactory = new WidgetFactory();
+    _modules.add(widgetFactory.createWidget('module', 'apple'));
+    _modules.add(widgetFactory.createWidget('module', 'orange'));
+    _modules.add(widgetFactory.createWidget('module', 'banana'));
+    _modules.add(widgetFactory.createWidget('module', 'grapefruit'));
+    _modules.add(widgetFactory.createWidget('module', 'blueberry'));
+  }
+
+  @override
+  void didUpdateWidget(ModuleManager oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Modules(_modules);
+  }
+}
